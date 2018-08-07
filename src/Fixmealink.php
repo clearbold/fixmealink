a05b6c698da115b14f90e870273e60576d47c12f
Mark Reeves <mjr@clearbold.com>
2018-08-07 19:18:09 -0400

    Setting things up

---
+++ src/Fixmealink.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link      https://github.com/clearbold/craft-fixmealink
+ * @copyright Copyright (c) Clearbold, LLC
+ */
+
+namespace clearbold\fixmealink;
+
+use craft\base\Plugin;
+// use craft\events\RegisterComponentTypesEvent;
+// use yii\base\Event;
+
+/**
+ * CmTransactionalAdapter implements a Campaign Monitor Transactional Email transport adapter into Craft’s mailer.
+ *
+ * @author Mark Reeves, Clearbold, LLC <hello@clearbold.com>
+ * @since 1.0
+ */
+class Fixmealink extends Plugin
+{
+    // Public Methods
+    // =========================================================================
+
+    public function init()
+    {
+        parent::init();
+    }
+}
